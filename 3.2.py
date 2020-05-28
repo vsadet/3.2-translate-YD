9bf974b74fe66b6cffa5379c9a6262a04a9d1421
Vitaliy <v.sadetdinov@yandex.ru>
2020-05-28 16:46:32 +0300

    Готовый код

--- 3.2.py
+++ 3.2.py
@@ -2,6 +2,7 @@ import requests
 
 
 def translate(text, lang, language):
+    """" Обращение к API Яндекс преводчика для перевода """
     URL = "https://translate.yandex.net/api/v1.5/tr.json/translate"  # куда отправляем запрос
     KEY = "trnsl.1.1.20200526T072738Z.e1dca6cc842663f6.37a8927e88ca396cb67ffbc65118b1d596982065"  # ключ полученный на сайте яндекса
     la = str(language + '-{}')  # переменная которая подменяет язык с которого надо перевести в отправляемых запросах
@@ -19,35 +20,32 @@ def translate(text, lang, language):
 
 
 def main(text, lang, language):  # приянли все 3 параметра
+    print("Текст для перевода: " + text)
     name_files = "Translated document " + language.upper() + ".txt"  # создали имя создаваемого документа
     with open(name_files, "w", encoding="utf-8") as f:  # открыли или создали файл для записи результата, указали параметр для ДОзаписи и кодировку и записали все это в переменную f
         f.write(translate(text, lang, language))  # Передаем в функцию 3 параметра. Полученный результат записываем в переменную f
-        download(name_files)
 
 
-def download(name_files):  # загрузка файлов на диск
-    # URL = "https://cloud-api.yandex.net/v1/disk/resources/upload?path=disk:/" + name_files  #
-    # path = "D:%5Cprogs%5Ctelegram%5C" + name_files  # путь куда на ЯД загрузить(корень)
-    # token = "AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A"
-    # path_computer = "D:\\progs\\telegram\\" + name_files  # путь где лежит файл на компьютере
-
+def download(name_files):
+    """ Загрузка файлов на диск """
     URL = "https://cloud-api.yandex.net/v1/disk/resources/upload"  #
-    path = "disk%5C" + name_files  # путь куда на ЯД загрузить(корень)
-    token = "AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A"
-    path_computer = "D:\\progs\\telegram\\" + name_files  # путь где лежит файл на компьютере
+    path = "/" + name_files  # путь куда на ЯД загрузить(корень)
+    token = "OAuth AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A"
 
     # Получаем ссылку для загрузки файла
-    params = {"path": path}
-    headers = {"Authorization": token}
+    params = {"path": path, 'overwrite': 'true'}
+    headers = {'Accept': 'application/json', "Authorization": token}
     res = requests.get(URL, params=params, headers=headers)
-    json_ = res.json()
-    URL_DOWNLOAD = json_["href"]
-    print("Ссылка для загрузки: " + json_["href"])
+    # json_ = res.json()
+    URL_DOWNLOAD = res.json().get('href')
+    print("Ссылка для загрузки: " + URL_DOWNLOAD)
 
     # Загружаем файл на диск
-    file = {name_files: open(path_computer, "rb")}
+    path_computer = 'D:\progs\\telegram\\' + name_files  # путь где лежит файл на компьютере
+    file = {"file": open(path_computer, "rb")}
     res_download = requests.put(URL_DOWNLOAD, files=file)
     print(res_download)
+    print(type(res_download))
 
 
 if __name__ == "__main__":
@@ -55,26 +53,7 @@ if __name__ == "__main__":
     for doc, language in docs.items():  # прогоняем каждый документ на перевод
         main(open(doc, encoding='utf-8').read(), "ru", language)  # отправляем с 3 параметрами(названием документа из словаря, язык на который перевести надо, и язык в файле)
 
+    for doc in docs:
+        name_files = "Translated document " + doc  # создали имя создаваемого документа
+        download(name_files)
 
-
-# token = "AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A"
-#
-# URL = "https://cloud-api.yandex.net/v1/disk/resources/upload?path=disk:/DE.txt"
-# path = "D:%5Cprogs%5Ctelegram%5CDE.txt"
-#
-# params = {
-#     "Authorization:": "AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A",
-#     "path": "D:%5Cprogs%5Ctelegram%5CDE.txt"
-#         }
-#
-# headers = {"Authorization": "AgAEA7qizIRBAADLW4R9aNCSREMag6JLwmvzj6A"}
-#
-# res = requests.get(URL, params=params, headers=headers)
-# json_ = res.json()
-#
-# URL_DOWNLOAD = json_["href"]
-# file = {"DE.txt": open("D:\\progs\\telegram\DE.txt", "rb")}
-# print(json_["href"])
-#
-# res_download = requests.put(URL_DOWNLOAD, file)
-# print(res_download)
